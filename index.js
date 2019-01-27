1ae81cb9233cb0c273b3a6dde3629df41f392895
Alexander Sterk <ajhsterk@gmail.com>
2019-01-27 16:49:19 +0100

    First attempt at adding Lightbulb type

--- index.js
+++ index.js
@@ -1,80 +1,87 @@
 var Olisto = require('olisto');
 var Service, Characteristic, HomebridgeAPI;
 
-module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    HomebridgeAPI = homebridge;
 
-  homebridge.registerPlatform('homebridge-olisto', 'Olisto', OlistoPlatform);
-  homebridge.registerAccessory('homebridge-olisto', 'OlistoAccessory', OlistoAccessory);
+    homebridge.registerPlatform('homebridge-olisto', 'Olisto', OlistoPlatform);
+    homebridge.registerAccessory('homebridge-olisto', 'OlistoAccessory', OlistoAccessory);
 };
 
 function OlistoPlatform(log, config) {
-  this.log = log;
-  this.triggs = config.triggs;
+    this.log = log;
+    this.triggs = config.triggs;
 }
 
-OlistoPlatform.prototype.accessories = function(callback) {
-  this.accessories = [];
+OlistoPlatform.prototype.accessories = function (callback) {
+    this.accessories = [];
 
-  for (var i = 0; i < this.triggs.length; i++) {
-    var olistoAccessory = new OlistoAccessory(this.log, this.triggs[i], this);
-    this.accessories.push(olistoAccessory);
-  }
+    for (var i = 0; i < this.triggs.length; i++) {
+        var olistoAccessory = new OlistoAccessory(this.log, this.triggs[i], this);
+        this.accessories.push(olistoAccessory);
+    }
 
-  callback(this.accessories);
-}
+    callback(this.accessories);
+};
 
 function OlistoAccessory(log, config, platform) {
-  this.platform = platform;
-  this.log = log;
-  this.name = config.name;
-  this.type = config.type || "push";
-  this.connecturl = config.connecturl
-  this.connector = new Olisto(this.connecturl);
+    this.platform = platform;
+    this.log = log;
+    this.name = config.name;
+    this.type = config.type || "push";
+    this.connecturl = config.connecturl;
+    this.connector = new Olisto(this.connecturl);
 
-  // Check for valid config
+    // Check for valid config
 
-  if (this.type != "push" && this.type != "switch" && this.type != "outlet") {
-    log.error('Invalid type, please use "push", "switch" or "outlet"!');
-    log.error('Now using default "push."')
-    this.type = "push"
-  }
+    if (this.type != "push" && this.type != "switch" && this.type != "outlet" && this.type != "light") {
+        log.error('Invalid type, please use "push", "switch", "light" or "outlet"!');
+        log.error('Now using default "push."');
+        this.type = "push"
+    }
 
-  if (this.type == "push" || this.type == "switch") {
-    this.service = new Service.Switch(this.name);
+    if (this.type == "push" || this.type == "switch") {
+        this.service = new Service.Switch(this.name);
 
-    this.service
-      .getCharacteristic(Characteristic.On)
-      //.on('get', this.getOn.bind(this))
-      .on('set', this.setOn.bind(this));
-  } else if (this.type == "outlet") {
-    this.service = new Service.Outlet(this.name)
+        this.service
+            .getCharacteristic(Characteristic.On)
+            //.on('get', this.getOn.bind(this))
+            .on('set', this.setOn.bind(this));
+    } else if (this.type == "outlet") {
+        this.service = new Service.Outlet(this.name);
 
-    this.service
-      .getCharacteristic(Characteristic.On)
-      //.on('get', this.getOn.bind(this))
-      .on('set', this.setOn.bind(this));
+        this.service
+            .getCharacteristic(Characteristic.On)
+            //.on('get', this.getOn.bind(this))
+            .on('set', this.setOn.bind(this));
 
-    this.service
-      .setCharacteristic(Characteristic.OutletInUse, true)
-  }
+        this.service
+            .setCharacteristic(Characteristic.OutletInUse, true)
+    } else if (this.type == "light") {
+        this.service = new Service.Lightbulb(this.name);
+
+        this.service
+            .getCharacteristic(Characteristic.On)
+            //.on('get', this.getOn.bind(this))
+            .on('set', this.setOn.bind(this));
+    }
 }
 
-OlistoAccessory.prototype.getInformationService = function() {
-  var informationService = new Service.AccessoryInformation();
-  informationService
-    .setCharacteristic(Characteristic.Name, this.name)
-    .setCharacteristic(Characteristic.Manufacturer, 'StefanNienhuis')
-    .setCharacteristic(Characteristic.Model, this.type.charAt(0).toUpperCase() + this.type.slice(1))
-    .setCharacteristic(Characteristic.SerialNumber, this.connecturl);
+OlistoAccessory.prototype.getInformationService = function () {
+    var informationService = new Service.AccessoryInformation();
+    informationService
+        .setCharacteristic(Characteristic.Name, this.name)
+        .setCharacteristic(Characteristic.Manufacturer, 'Alexander Sterk')
+        .setCharacteristic(Characteristic.Model, this.type.charAt(0).toUpperCase() + this.type.slice(1))
+        .setCharacteristic(Characteristic.SerialNumber, this.connecturl);
 
-  return informationService;
-}
+    return informationService;
+};
 
-OlistoAccessory.prototype.getServices = function() {
-  return [this.service, this.getInformationService()];
+OlistoAccessory.prototype.getServices = function () {
+    return [this.service, this.getInformationService()];
 };
 
 /* Requested feature: get value of connector from Olisto
@@ -83,19 +90,19 @@ OlistoAccessory.prototype.getOn = function(callback) {
 }
 */
 
-OlistoAccessory.prototype.setOn = function(on, callback) {
-  var accessory = this;
+OlistoAccessory.prototype.setOn = function (on, callback) {
+    var accessory = this;
 
-  if (accessory.type == "push") {
-    // Accessory is a push button
-    if (+on == 1) {
-      accessory.connector.setOn();
-      callback(accessory.service.setCharacteristic(Characteristic.On, 0));
+    if (accessory.type == "push") {
+        // Accessory is a push button
+        if (+on == 1) {
+            accessory.connector.setOn();
+            callback(accessory.service.setCharacteristic(Characteristic.On, 0));
+        }
+
+    } else if (accessory.type == "switch" || accessory.type == "outlet" || accessory.type == "light") {
+        // Accessory is an switch or outlet
+        accessory.connector.setValue(this.name + +on);
+        callback(null);
     }
-
-  } else if (accessory.type == "switch" || accessory.type == "outlet") {
-    // Accessory is an switch or outlet
-    accessory.connector.setValue(this.name + +on);
-    callback(null);
-  }
-}
+};
